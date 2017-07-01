b98ba4422e782b9a44ec88a9b318d186d615ba55
George Aidonidis <george.aidonidis@gmail.com>
2017-07-01 18:58:01 +0300

    Add enzyme for frontend testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+describe('<App/>', () => {
+  it('exists', () => {
+    expect(App).to.exist;
+  });
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper).to.have.length(1);
+  });
 });
